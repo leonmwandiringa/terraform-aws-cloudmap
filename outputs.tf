717baa5605277959fbb345d8bb928dc6bb827fb0
Leon Mwandiringa <leonmwandiringa@gmail.com>
2024-08-04 06:26:54 +0200

    vars

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "id"
-  value       = var.service_connect_configuration.vpc_id == null ? aws_service_discovery_public_dns_namespace.default[0].id : aws_service_discovery_private_dns_namespace.default[0].id
+  value       = var.vpc_id == null ? aws_service_discovery_public_dns_namespace.default[0].id : aws_service_discovery_private_dns_namespace.default[0].id
 }
 
 output "arn" {
   description = "ARN"
-  value       = var.service_connect_configuration.vpc_id == null ? aws_service_discovery_public_dns_namespace.default[0].arn : aws_service_discovery_private_dns_namespace.default[0].arn
+  value       = var.vpc_id == null ? aws_service_discovery_public_dns_namespace.default[0].arn : aws_service_discovery_private_dns_namespace.default[0].arn
 }
 
 output "hosted_zone" {
   description = "zone"
-  value       = var.service_connect_configuration.vpc_id == null ? aws_service_discovery_public_dns_namespace.default[0].hosted_zone : aws_service_discovery_private_dns_namespace.default[0].hosted_zone
+  value       = var.vpc_id == null ? aws_service_discovery_public_dns_namespace.default[0].hosted_zone : aws_service_discovery_private_dns_namespace.default[0].hosted_zone
 }
